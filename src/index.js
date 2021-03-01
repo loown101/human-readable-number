ddb622f22bd92fcccc05783b0e2c9cdc9ae3f67c
loown101 <loown101@gmail.com>
2021-03-01 15:02:04 +0300

    fix: error correction in function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 let arr = [
-        ["zero", "one", "two", "three", "four", "five", "six",
-        "seven", "eight", "nine", " ten", " eleven",
-        "twelve", " thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", " eighteen", "nineteen"],
-        
-        ["","","twenty", "thirty", "forty", "fifty", "sixty",
-        "seventy", "eighty", "ninety"],
-        
-        ["","one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"]
-        ]; 
-        
+    ["", " one", " two", " three", " four", " five", " six",
+    " seven", " eight", " nine", " ten", " eleven",
+    " twelve", " thirteen", " fourteen", " fifteen",
+    " sixteen", " seventeen", " eighteen", " nineteen"],
+    
+    ["",""," twenty", " thirty", " forty", " fifty", " sixty",
+    " seventy", " eighty", " ninety"],
+    
+    ["","one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"]
+]
+
         module.exports = function toReadable (number) {
-    if (num < 20) {
-        return arr[0][num];
-        } else if (20 <= num && num <= 100) {
-        return (arr[1][parseInt(num / 10)] + ' ' + arr[0][num % 10]);
-        } else {
-        return (arr[2][parseInt(num / 100)] + ' ' + arr[1][parseInt(num / 10) % 10] + ' ' + arr[0][num % 10]);
+            number = parseInt(number, 10);
+            if (number == 0) {
+                return 'zero'
+            } else if (number < 20) {
+                return arr[0][number].trim();
+            } else if (number > 19 && number < 100) {
+                return (arr[1][parseInt(number / 10) % 10] + arr[0][number % 10]).trim();
+            } else {
+                let n = parseInt(number/10) % 10;
+                if (n == 1) {
+                    return (arr[2][parseInt(number / 100) % 100] + arr[0][number % 100]).trim();
+                } else {
+                    return (arr[2][parseInt(number / 100) % 100] + arr[1][parseInt(number / 10) % 10]+ arr[0][number % 10]).trim();
+                }
+            }
         }
-}
