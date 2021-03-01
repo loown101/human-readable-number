f9f767c29a4988a752e9ccf6c2ba494e31ed97ac
loown101 <loown101@gmail.com>
2021-03-01 12:36:18 +0300

    init: starthuman-readable-number-tack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function toReadable (number) {
-  
+let arr = [
+        ["zero", "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine", " ten", " eleven",
+        "twelve", " thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", " eighteen", "nineteen"],
+        
+        ["","","twenty", "thirty", "forty", "fifty", "sixty",
+        "seventy", "eighty", "ninety"],
+        
+        ["","one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"]
+        ]; 
+        
+        module.exports = function toReadable (number) {
+    if (num < 20) {
+        return arr[0][num];
+        } else if (20 <= num && num <= 100) {
+        return (arr[1][parseInt(num / 10)] + ' ' + arr[0][num % 10]);
+        } else {
+        return (arr[2][parseInt(num / 100)] + ' ' + arr[1][parseInt(num / 10) % 10] + ' ' + arr[0][num % 10]);
+        }
 }
